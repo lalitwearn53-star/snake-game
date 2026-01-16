b38ef1122a6a332ffdd667029a0785f9eb9eca85
lalitwearn53-star <lalitwearn53@gmail.com>
2026-01-16 23:23:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const box = 20;
-const canvasSize = 500;
+const canvasSize = 600;
 
 let snake;
 let direction;
